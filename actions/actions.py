b70cea9ee50f8a4033611fdd589c1cd321aba647
29922-pjcarriel <pjcarriel@espe.edu.ec>
2025-11-03 09:43:27 -0500

    Cambios

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,17 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+# actions.py
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+        # Muestra el mensaje de bienvenida automático
+        dispatcher.utter_message(text="👋 ¡Hola! Soy tu asistente de comandos Linux. Puedo ayudarte a aprender los comandos más útiles del sistema. 😊")
+
+        return []
